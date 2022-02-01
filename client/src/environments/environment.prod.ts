062f31e99d3e27e051c2a6e19ec15539c23656bf
Sacred97 <denoon97@gmail.com>
2022-02-01 13:59:44 +0300

    changes

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  daDataConfig: {
+    apiKey: '0124fbdc0ad6a70ba2e03f5b3f2399fb935a60db',
+    addressUrl: 'https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/address',
+    companyUrl: 'https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/party'
+  }
 };
